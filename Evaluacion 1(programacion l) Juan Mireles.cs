a0cd3515a08348d8d3f5eb19db46848fa2c4e29a
JuanCa3K <78376936+JuanCa3K@users.noreply.github.com>
2021-02-02 22:49:24 -0400

    Add files via upload

---
+++ Evaluacion 1(programacion l) Juan Mireles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int a = 2;
+		int b = 4;
+		int c = 1;
+
+		//aacb
+	    Console.WriteLine("aacb");
+	    Console.Write(a);
+		Console.Write(a);
+		Console.Write(c);
+		Console.WriteLine(b);
+			
+		Console.WriteLine("----");
+
+		//bca
+		Console.WriteLine("bca");
+		Console.Write(b);
+		Console.Write(c);
+		Console.WriteLine(a);
+
+		Console.WriteLine("----");
+		
+		//bcbc
+		Console.WriteLine("bcbc");
+	    Console.Write(b);
+		Console.Write(c);
+		Console.Write(b);
+		Console.WriteLine(c);
+		
+		//Finalizado
+		
+        }
+    }
+}
